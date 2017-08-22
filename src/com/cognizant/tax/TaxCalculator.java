919161ae03c87bbd12cf83979fe283722f8ea3e3
esraa <esraa.alaarag@gmail.com>
2017-08-22 15:12:59 -0400

    done again

--- src/com/cognizant/tax/TaxCalculator.java
+++ src/com/cognizant/tax/TaxCalculator.java
@@ -3,21 +3,22 @@ package com.cognizant.tax;
 public class TaxCalculator {
 	float basicSalary ;
 	boolean citizenship ;
+	float tax;
 	TaxCalculator(boolean c , float salary){
 		this.basicSalary=salary;
 		this.citizenship=c;
 	}
-	float calculateTax () {
-		float tax = 30*this.basicSalary/100;
+	void calculateTax () {
+	 this.tax = 30*this.basicSalary/100;
 	System.out.println("The Tax of the employee  for  the "+ (int)this.basicSalary + " is " +  (int)tax);
-	return tax;
+	
 	}
 	
-	int deductTax() {
-		float tax=this.calculateTax();
-		int nettSalary=(int)(this.basicSalary-tax);
-	    System.out.println("The nett salary of the employee "+ (int)nettSalary + "\n" );
-		return nettSalary;
+	void deductTax() {
+		
+		int nettSalary=(int)(this.basicSalary-this.tax);
+	    System.out.println("The nett salary of the employee "+ (int)nettSalary );
+		
 	}
 	void validateSalary() {
 		boolean response = false;
@@ -25,6 +26,6 @@ public class TaxCalculator {
 		{
 			response=true;
 		}
-		System.out.println("The salary and citizenship eligibility:  "+response);
+		System.out.println("The salary and citizenship eligibility:  "+response+ "\n" );
 	}
 }
